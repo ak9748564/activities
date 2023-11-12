e11407c43f57ef9e41ba1bb7c21d816756a82335
ak9748564 <122973008+ak9748564@users.noreply.github.com>
2023-11-12 16:36:31 +0530

    Delete src directory

--- src/redux/constants.js
+++
@@ -1,3 +0,0 @@
-export const BASE_URL_NODEJS = 'https://api-node-app.gtftech.com/api/v1';
-export const BASE_URL_DOTNET = 'https://api-app.gtftech.com/';
-// export const BASE_URL_NODEJS = 'https://192.168.68.130:3030/api/v1';
