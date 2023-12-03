fc88638c09bad8dab61921934d3334f5b1f2939c
ak9748564 <122973008+ak9748564@users.noreply.github.com>
2023-12-03 16:59:25 +0530

    Update Home.jsx

--- src/activities/components/Home.jsx
+++ src/activities/components/Home.jsx
@@ -62,11 +62,20 @@ export default function Home() {
   return (
     <div className='h-[100dvh] w-screen'>
       <div className='w-full h-full flex items-center justify-center bg-cover' style={{backgroundImage:`url(${uae_mobile})`}}>
-        <div className='w-full h-full flex flex-col justify-evenly items-center backdrop-brightness-50 p-10'>
+        <div className='w-full h-full flex flex-col justify-center items-center backdrop-brightness-50 p-10'>
           <p className='text-green-400 text-[20px] absolute left-4 top-4'>Galaxy Exhibitions</p>
+          <p className='absolute right-4 top-4 cursor-pointer'>
+            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-list fill-green-400" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
+            </svg>
+          </p>
           <p className='text-[25px] text-white/80'>Get Your Business in UAE</p>
-          <button className='h-[40px] rounded-full w-[200px] leading-[40px] bg-white text-center text-black shadow-xl'>
-            <Link to='activities-list'>See Activities</Link>
+          <button className='h-[40px] rounded-full w-[200px] leading-[40px] bg-white text-center text-black shadow-xl hover:text-white/90 hover:bg-green-500 transition-all flex items-center justify-center gap-1 mt-6'>
+            <Link to='activities-list'>
+              See Activities</Link>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="20" fill="currentColor" class="bi bi-arrow-right transition-all" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"/>
+            </svg>
           </button>
         </div>
       </div>
