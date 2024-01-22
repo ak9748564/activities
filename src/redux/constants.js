2ae5806e292f1b6367fc1aba8506a886ae54f1eb
ak9748564 <122973008+ak9748564@users.noreply.github.com>
2024-01-22 21:04:33 +0530

    Delete src directory

--- src/redux/constants.js
+++
@@ -1,5 +0,0 @@
-export const BASE_URL_NODEJS = 'https://api-node-app.gtftech.com/api/v1';
-export const BASE_URL_DOTNET = 'https://api-app.gtftech.com/';
-// export const BASE_URL_NODEJS = 'https://192.168.68.130:3030/api/v1';
-// export const BASE_URL_ACT = 'http://localhost:4000';
-export const BASE_URL_ACT = 'https://galaxy-exhibitions-activities.onrender.com';
